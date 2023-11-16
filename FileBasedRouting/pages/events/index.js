bd352c3fc04b607e3c0c337b9c342a75cd4c573b
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-16 13:00:57 +0530

    layout and header added

--- FileBasedRouting/pages/events/index.js
+++ FileBasedRouting/pages/events/index.js
@@ -1,9 +1,13 @@
-function AllEventsPage(){
-    return(
-        <div>
-            <h1>All events</h1>
-        </div>
-    )
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/data/dummy-data";
+
+function AllEventsPage() {
+  const events = getAllEvents();
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 }
 
-export default AllEventsPage
+export default AllEventsPage;
