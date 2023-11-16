b5968b972c558596d7cf05371ccfc6d3a7c85e77
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-16 09:28:05 +0530

    some dummy data added

---
+++ FileBasedRouting/pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FilteredEventPage() {
+  return (
+    <div>
+      <h1>Filterd events</h1>
+    </div>
+  )
+}
+
+export default FilteredEventPage
--- FileBasedRouting/pages/index.js
+++ FileBasedRouting/pages/index.js
@@ -1,22 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-const inter = Inter({ subsets: ['latin'] })
+import { getFeaturedEvents } from "@/data/dummy-data"
+
 
 export default function Home() {
+  const featuredEvents=getFeaturedEvents()
   return (
     <div>
      <h2>This is Home Page</h2>
-     <ul>
-      <li>
-        <Link href="/portfolio">Portfolio</Link>
-      </li>
-      <li>
-        <Link href="/clients">Clients</Link>
-      </li>
-     </ul>
     </div>
   )
 }
