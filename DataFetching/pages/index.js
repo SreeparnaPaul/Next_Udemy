d7ddb2340bccb0c495c7827b155fd3c103d3b3d2
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-17 11:19:35 +0530

    Incremental Static Regeneration

--- DataFetching/pages/index.js
+++ DataFetching/pages/index.js
@@ -1,11 +1,38 @@
+import fs from "fs/promises";
+import path from "path";
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
-
+//first this function will get called then the component
+export async function getStaticProps() {
+  console.log("(Re)-generating..");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return {
+      notFound: true, //404 page will reflect
+    };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10, //In case of development page will be regenated on every request but in case of production, after 10 secs page will regenerate
+  };
+}
 export default HomePage;
