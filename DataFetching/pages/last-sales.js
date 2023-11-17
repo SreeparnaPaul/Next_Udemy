860e619b5690cf32fa7a4db8adbf596862fa8333
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-17 18:30:54 +0530

    useSWR

--- DataFetching/pages/last-sales.js
+++ DataFetching/pages/last-sales.js
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
+  // const [isLoading, setIsLoading] = useState(false);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
 
-function LastSalesPage() {
-  const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
+  const { data, error } = useSWR(
+    "https://nextjs-course-27cb7-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      "https://nextjs-course-27cb7-default-rtdb.firebaseio.com/sales.json" //.json is firebase requirement
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
+    if (data) {
+      //as i am using firebase realtime database, so to get the data in a structured way the below code is required
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+  // if (isLoading) {
+  //   return <p>Loading ...</p>;
+  // }
+
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+  if (!data && !sales) {
     return <p>Loading ...</p>;
   }
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+
   return (
     <ul>
       {sales.map((sale) => (
@@ -39,5 +44,24 @@ function LastSalesPage() {
     </ul>
   );
 }
-
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-27cb7-default-rtdb.firebaseio.com/sales.json" //.json is firebase requirement
+  );
+  const data = await response.json();
+  const transformedSales = [];
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return {
+    props: {
+      sales: transformedSales,
+    },
+    revalidate: 10,
+  };
+}
 export default LastSalesPage;
