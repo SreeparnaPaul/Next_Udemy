8ede685cc281503f7c756eeea6303a78f618a14c
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-21 00:29:58 +0530

    POST API

--- APIRoute/pages/index.js
+++ APIRoute/pages/index.js
@@ -1,14 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
-const inter = Inter({ subsets: ["latin"] });
+import { useRef } from "react";
 
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  };
   return (
     <div>
       <h2>This is Home Page</h2>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
