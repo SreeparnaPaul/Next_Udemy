73556b20b683752ff15a419e32b78a899b5c140a
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-20 23:59:25 +0530

    API Route Added

---
+++ APIRoute/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <h2>This is Home Page</h2>
+    </div>
+  );
+}
