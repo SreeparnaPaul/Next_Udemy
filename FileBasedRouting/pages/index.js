5ef792eb2f5f34fe80299a928380371a55bcc99f
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-21 13:48:26 +0530

    input folder added

--- FileBasedRouting/pages/index.js
+++ FileBasedRouting/pages/index.js
@@ -1,4 +1,5 @@
 import EventList from "@/components/events/event-list";
+import NewsletterRegistration from "@/components/input/newsletter-registration";
 import { getFeaturedEvents } from "@/helpers/api-util";
 import Head from "next/head";
 import Link from "next/link";
@@ -12,6 +13,7 @@ function HomePage(props) {
           content="Find a lot of great events that allow you to evolve ..."
         />
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events} />
     </div>
   );
