f6d56ec4d1f36c13fc2d69606a76a1b30b3b6924
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-21 19:10:41 +0530

    db connection

--- FileBasedRouting/pages/api/comments/[eventId].js
+++ FileBasedRouting/pages/api/comments/[eventId].js
@@ -1,5 +1,14 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
   const eventId = req.query.eventId;
+  const client = new MongoClient(
+    "mongodb+srv://sreeparnapaul21:sreeparna@cluster0.9fu3q59.mongodb.net/events?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
   if (req.method === "POST") {
     const { email, name, comment } = req.body;
     if (
@@ -13,21 +22,31 @@ function handler(req, res) {
       res.status(422).json({ message: "Invalid Input" });
       return;
     }
-    console.log(email, name, comment);
     const newComment = {
-      id: new Date().toISOString(),
       email,
       name,
       comment,
+      eventId,
     };
+    await client.connect();
+
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
+
+    console.log(result);
+    newComment.id = result.insertedId;
+
     res.status(201).json({ message: "Added Comment", comment: newComment });
   }
   if (req.method === "GET") {
-    const dummyList = [
-      { id: "c1", name: "Sree", comment: "First comment" },
-      { id: "c2", name: "Parna", comment: "Second comment" },
-    ];
-    res.status(200).json({ comments: dummyList });
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+    res.status(200).json({ comments: documents });
   }
+  client.close();
 }
 export default handler;
--- FileBasedRouting/pages/api/newsletter.js
+++ FileBasedRouting/pages/api/newsletter.js
@@ -1,12 +1,48 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function connectDatabase() {
+  const client = new MongoClient(
+    "mongodb+srv://sreeparnapaul21:sreeparna@cluster0.9fu3q59.mongodb.net/events?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
+
+  await client.connect();
+  return client;
+}
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
-    console.log(userEmail);
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "Connecting to the database failed" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Inserting document to database failed!" });
+      return;
+    }
+
     res.status(201).json({ message: "Signed Up!" });
   }
 }
+
 export default handler;
