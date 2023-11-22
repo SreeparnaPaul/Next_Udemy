cbf21bac3b0d26995327c5f11231c101089af8c9
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-22 11:42:29 +0530

    Notification

--- FileBasedRouting/components/layout/layout.js
+++ FileBasedRouting/components/layout/layout.js
@@ -1,10 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import MainHeader from "./main-header";
+import NotificationContext from "@/store/notification-context";
+import Notification from "../ui/notification";
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
