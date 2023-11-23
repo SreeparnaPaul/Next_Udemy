bcc93c2d8598883eb43c583e1ddb9316b88160b0
Sreeparna Paul <sreeparnapaul21@gmail.com>
2023-11-23 12:56:19 +0530

    All-posts

--- blog_application/pages/posts/index.js
+++ blog_application/pages/posts/index.js
@@ -1,2 +1,35 @@
-function AllPostsPage() {}
+import AllPosts from "@/components/posts/all-posts";
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-next.js",
+    title: "Getting Started with Next Js",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React Framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-next.js2",
+    title: "Getting Started with Next Js",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React Framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-next.js3",
+    title: "Getting Started with Next Js",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React Framework",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-next.js4",
+    title: "Getting Started with Next Js",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJs is a React Framework",
+    date: "2022-02-10",
+  },
+];
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 export default AllPostsPage;
